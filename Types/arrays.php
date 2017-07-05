b254d9b31b8be68b4e91c9f93dc754a7d075d3a8
begimov <begimov@gmail.com>
2017-07-05 19:10:29 +0300

    arrays digging

--- Types/arrays.php
+++ Types/arrays.php
@@ -41,3 +41,70 @@ $arr3[] = 4;
 echo var_dump($arr3).BR;
 $arr3 = array_values($arr3);
 echo var_dump($arr3).BR;
+echo count($arr3).BR;
+$index = 0;
+echo $arr3["$index"].BR;
+
+$arr = array('fruit' => 'apple', 'veggie' => 'carrot');
+echo $arr[fruit].BR;
+const fruit = 'veggie';
+echo $arr[fruit].BR;
+echo "$arr[fruit]".BR;
+echo "{$arr[fruit]}".BR;
+print_r((array)'text00');
+print_r(array('text00'));
+echo BR;
+
+class A {
+    private $A0 = 0;
+    private $A1;
+    protected $A2;
+    public $A3;
+}
+var_dump((array) new A);
+var_dump((array)null);
+echo BR;
+
+$colors = array('red', 'blue', 'green', 'yellow');
+foreach ($colors as $color) {
+    echo "$color ";
+}
+
+foreach ($colors as &$color) {
+    $color = strtoupper($color);
+}
+unset($color);
+
+// foreach ($colors as $key => $value) {
+//     $colors[$key] = strtoupper($value);
+// }
+
+foreach ($colors as $color) {
+    echo "$color ";
+}
+
+$handle = opendir('..');
+var_dump($handle);
+while (($file = readdir($handle)) !== false) {
+    $files[] = $file;
+}
+closedir($handle);
+var_dump($files);
+
+$arr = ['arr1' => [1,2,3], 'arr2' => [4,5,6]];
+var_dump($arr['arr2'][0]);
+$juices["apple"]["green"] = "good";
+var_dump($juices);
+echo BR;
+
+$arr1 = [1,2];
+$arr2 = &$arr1;
+$arr2[] = 2;
+$arr1[] = 1;
+var_dump($arr1, $arr2);
+echo BR;
+
+$a = 1;
+$b = $a;
+$a = 2;
+var_dump($a, $b);

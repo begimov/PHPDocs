25bf314756470a28c5cc70322211b226d99eef0a
begimov <begimov@gmail.com>
2017-07-08 11:45:11 +0300

    control structures for diggin

--- ControlStructures/index.php
+++ ControlStructures/index.php
@@ -19,6 +19,35 @@ do {
     $i++;
 } while ($i < 2);
 
-for ($i=0; $i < 1; $i++) { 
-  # code...
+for ($i=0; $i < 1; $i++) {
+  echo "string" . $i . BR;
 }
+
+for ($i=1; ; $i++) {
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+}
+echo BR;
+
+$i = 1;
+for (; ; ) {
+    if ($i > 10) break;
+    echo $i;
+    $i++;
+}
+echo BR;
+
+for ($i=1, $j=0; $i <= 10; $j += $i, print $i, $i++);
+
+$people = [
+  ['name' => 'Andrew', 'salt' => 1],
+  ['name' => 'Sasha', 'salt' => 2]
+];
+
+for ($i=0, $size = count($people); $i < $size; ++$i) {
+    $people[$i]['salt'] = mt_rand(000000, 999999);
+}
+echo '<pre>';
+var_dump($people);
